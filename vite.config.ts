fd2bf59616650ff96ae5d8738b2ea635670b54ef
Dpalme <diegoPalmerin24@gmail.com>
2023-07-03 01:44:29 -0600

    autodeploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), WindiCSS()],
-  server: {
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
-      '@shared': path.resolve(
-        path.dirname(fileURLToPath(import.meta.url)),
-        './src/shared'
-      ),
-      '@assets': path.resolve(
-        path.dirname(fileURLToPath(import.meta.url)),
-        './src/assets'
-      ),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), WindiCSS()],
+    server: {
+      open: true,
     },
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          './src'
+        ),
+        '@shared': path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          './src/shared'
+        ),
+        '@assets': path.resolve(
+          path.dirname(fileURLToPath(import.meta.url)),
+          './src/assets'
+        ),
+      },
+    },
+    base: env.BASE_URL || '',
+  };
 });
