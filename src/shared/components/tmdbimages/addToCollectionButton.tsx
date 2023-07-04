cd989a9a2ef8965a039ea3f6b1cd36e0fe33c9f5
Dpalme <diegoPalmerin24@gmail.com>
2023-07-03 23:10:26 -0600

    component to toggle image collection status

---
+++ src/shared/components/tmdbimages/addToCollectionButton.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { LoadingSpinner } from '../loadingSpinner';
+import {
+  useAddToCollection,
+  useRemoveFromCollection,
+} from '@/collection/hooks';
+import AddIcon from '@assets/heart_outline.svg';
+import AddedIcon from '@assets/heart_filled.svg';
+
+export const AddToCollectionButton = (props: {
+  type: 'backdrop' | 'logo' | 'poster' | 'profile' | 'still';
+  movie_id: number;
+  file_path: string;
+  inCollection: boolean;
+  aspect_ratio: number;
+  height: number;
+  width: number;
+}) => {
+  const [inCollection, setInCollection] = useState(props.inCollection);
+  const { mutate: addToCollection, isLoading: addLoading } =
+    useAddToCollection();
+  const { mutate: removeFromCollection, isLoading: remLoading } =
+    useRemoveFromCollection();
+
+  return (
+    <div className="w-8 h-8">
+      <div
+        onClick={(ev) => {
+          (inCollection ? removeFromCollection : addToCollection)(
+            {
+              file_path: props.file_path,
+              movie_id: props.movie_id,
+              aspect_ratio: props.aspect_ratio,
+              height: props.height,
+              width: props.width,
+              type: props.type,
+            },
+            {
+              onSuccess: () => setInCollection((prevValue) => !prevValue),
+            }
+          );
+          ev.stopPropagation();
+        }}
+        className={[
+          'cursor-pointer h-full',
+          addLoading || (remLoading && 'animate-pulse'),
+        ].join(' ')}
+        key={`${props.file_path}.button.${inCollection}`}
+      >
+        <img
+          className="w-full h-full"
+          src={inCollection ? AddedIcon : AddIcon}
+        />
+      </div>
+    </div>
+  );
+};
