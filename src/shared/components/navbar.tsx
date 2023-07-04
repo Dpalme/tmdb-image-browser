2ac5b1c992b7b8d7cf2ad6228099d3b1a9734bae
Dpalme <diegoPalmerin24@gmail.com>
2023-07-03 23:07:24 -0600

    added icons

--- src/shared/components/navbar.tsx
+++ src/shared/components/navbar.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { m, useReducedMotion } from 'framer-motion';
+import CollectionIcon from '@assets/folder.svg';
+import { SearchField } from './searchField';
 
 export const Navbar = () => {
   const shouldReduceMotion = useReducedMotion();
@@ -9,27 +11,22 @@ export const Navbar = () => {
       pl-12 py-4 md:px-4 bottom-0 md:top-0
       md:bottom-auto right-0 md:left-0
       flex-col md:flex-row backdrop-blur-lg backdrop-filter
-      w-full z-20 bg-black bg-opacity-10"
+      w-full z-20 dark:(bg-black text-white) bg-white text-black"
       initial={{ y: shouldReduceMotion ? 0 : '-100%' }}
       animate={{ y: 0 }}
       transition={{ duration: 1 }}
     >
-      <NavLink to="/" className="font-title text-sm font-extrabold navlink">
+      <NavLink to="/" className="font-title text-lg navlink">
         POSTERIFY
       </NavLink>
-      <div className="flex gap-6 md:gap-4">
-        <NavLink
-          to="/search"
-          className="font-title lowercase md:text-base text-xl drop-shadow-lg navlink"
-        >
-          search
-        </NavLink>
+      <div className="flex gap-6 md:gap-4 items-center">
         <NavLink
           to="/collection"
-          className="font-title lowercase md:text-base text-xl drop-shadow-lg navlink"
+          className="lowercase md:text-base text-xl drop-shadow-lg navlink"
         >
-          collection
+          <img src={CollectionIcon} alt="Collection" />
         </NavLink>
+        <SearchField />
       </div>
     </m.nav>
   );
