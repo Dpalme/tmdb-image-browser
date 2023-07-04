b3f3657ca7d7b36bbe4c8bfeed6d837a8851b2a4
Dpalme <diegoPalmerin24@gmail.com>
2023-07-03 23:05:04 -0600

    infinite scrolling homepage

--- src/home/services.ts
+++ src/home/services.ts
@@ -1,6 +1,8 @@
 import { useTMDB } from '../shared/hooks/useTMDB';
 import { ITMDBMovie } from '../shared/models/movie';
 
-export async function getFeed(): Promise<IPage<ITMDBMovie>> {
-  return useTMDB('/3/movie/popular') as unknown as Promise<IPage<ITMDBMovie>>;
+export async function getFeed(page: number): Promise<IPage<ITMDBMovie>> {
+  return useTMDB('/3/movie/popular?page=' + page) as unknown as Promise<
+    IPage<ITMDBMovie>
+  >;
 }
