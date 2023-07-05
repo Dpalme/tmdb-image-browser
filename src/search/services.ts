6ca42eb8b2f1b035143778440c581c04597165ab
Dpalme <diegoPalmerin24@gmail.com>
2023-07-04 23:27:33 -0600

    changed search page to single page

--- src/search/services.ts
+++ src/search/services.ts
@@ -2,10 +2,17 @@ import { useTMDB } from '../shared/hooks/useTMDB';
 import { ITMDBMovie } from '../shared/models/movie';
 
 export async function getSearch(
-  query: string,
+  query: string = '',
   page: number = 1
 ): Promise<IPage<ITMDBMovie>> {
-  return useTMDB(
-    '/3/search/movie?query=' + query + '&page=' + page
+  if (query === '')
+    return { page: 1, total_pages: 1, results: [], total_results: 0 };
+  return useTMDB('/3/search/movie?query=' + query + '&page=' + page).then(
+    (data: IPage<ITMDBMovie>) => {
+      data.results = data.results.filter(
+        (a) => !!a.poster_path && !!a.backdrop_path
+      );
+      return data;
+    }
   ) as unknown as Promise<IPage<ITMDBMovie>>;
 }
