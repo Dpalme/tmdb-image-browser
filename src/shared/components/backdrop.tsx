6f3beb784a2371568ecb332e8dc6e690e0476840
Dpalme <diegoPalmerin24@gmail.com>
2023-07-05 00:23:09 -0600

    updated loading screen to use logo

--- src/shared/components/backdrop.tsx
+++ src/shared/components/backdrop.tsx
@@ -1,16 +1,54 @@
-import { LoadingSpinner } from './loadingSpinner';
-
 export const LoadingBackdrop = () => {
   return (
     <div
       className="fixed z-10 w-full h-full text-main flex
-      flex-col items-center justify-center gap-6"
+      flex-col items-center justify-center gap-16"
     >
-      <p className="text-6xl pb-2">
-        P
-        <span className="spin" />
-        STERIFY
-      </p>
+      <svg
+        width="320"
+        height="320"
+        viewBox="0 0 60 60"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect x="9.81" y="15" width="19.8" height="30" rx="2" fill="#64D2FF">
+          <animate
+            attributeName="y"
+            values="15;10;15;15;15;15;15;15;15"
+            dur="1.5s"
+            repeatCount="indefinite"
+            keySplines="0 0 2 1; 3 0 4 1; 0.5 7 0.5 1; 4 2 0.5 1"
+          />
+        </rect>
+        <rect x="11.61" y="12.5" width="23.1" height="35" rx="2" fill="#FF9F0A">
+          <animate
+            attributeName="y"
+            values="12.5;12.5;10;12.5;12.5;12.5;12.5;12.5;12.5"
+            dur="1.5s"
+            repeatCount="indefinite"
+            keySplines="0 0 2 1; 3 0 4 1; 0.5 7 0.5 1; 4 2 0.5 1"
+          />
+        </rect>
+        <rect x="16.71" y="11" width="25.08" height="38" rx="2" fill="#FFD60A">
+          <animate
+            attributeName="y"
+            values="11;11;11;9;11;11;11;11;11"
+            dur="1.5s"
+            repeatCount="indefinite"
+            keySplines="0 0 2 1; 3 0 4 1; 0.5 7 0.5 1; 4 2 0.5 1"
+          />
+        </rect>
+        <rect x="23" y="10" width="26.4" height="40" rx="2" fill="#30D158">
+          <animate
+            attributeName="y"
+            values="10;10;10;10;8;10;10;10;10"
+            dur="1.5s"
+            repeatCount="indefinite"
+            keySplines="0 0 2 1; 3 0 4 1; 0.5 7 0.5 1; 4 2 0.5 1"
+          />
+        </rect>
+      </svg>
+      <p className="text-3xl pb-2 font-title inline-block">POSTERIFY</p>
     </div>
   );
 };
