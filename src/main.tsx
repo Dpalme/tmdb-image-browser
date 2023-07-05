aa8048b90588ed17e4417bfac670a6aaf6c1bc55
Dpalme <diegoPalmerin24@gmail.com>
2023-07-04 23:14:38 -0600

    removed framer-motion for performance reasons

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { LazyMotion } from 'framer-motion';
 import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,17 +8,13 @@ import './shared/providers/database';
 import QueryProvider from './shared/providers/queryProvider';
 
 const App = lazy(() => import('./App'));
-const UsedAnimations = () =>
-  import('./shared/animations').then((a) => a.default);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryProvider>
       <Suspense fallback={<LoadingBackdrop />}>
         <BrowserRouter basename={import.meta.env.BASE_URL}>
-          <LazyMotion features={UsedAnimations} strict>
-            <App />
-          </LazyMotion>
+          <App />
         </BrowserRouter>
       </Suspense>
     </QueryProvider>
