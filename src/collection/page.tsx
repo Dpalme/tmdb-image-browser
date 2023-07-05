5005a712c4175c103e9cf61b3be62563dc76f651
Dpalme <diegoPalmerin24@gmail.com>
2023-07-04 23:13:46 -0600

    added tutorial on empty collection

--- src/collection/page.tsx
+++ src/collection/page.tsx
@@ -1,7 +1,8 @@
 import { useGetCollection } from './hooks';
 import { HandleAsync } from '@/shared/components/handleAsync';
 import { Container } from '@/shared/components/container';
-import { ImageCard } from '@/movie/imageCard';
+import { ImageCard } from '@/shared/components/tmdbimages/imageCard';
+import { NavLink } from 'react-router-dom';
 
 export const CollectionPage = () => {
   const { collection, isLoading, error } = useGetCollection();
@@ -10,11 +11,40 @@ export const CollectionPage = () => {
     <HandleAsync loading={isLoading} error={error}>
       <Container>
         <div className="grid grid-cols-1 gap-2">
+          {!collection ||
+            (collection.length == 0 && (
+              <>
+                <h1 className="text-2xl mb-4">
+                  You haven't collected anything!
+                </h1>
+                <p>
+                  Try going to the{' '}
+                  <NavLink to="/" className="font-extrabold text-emerald-600">
+                    homepage
+                  </NavLink>{' '}
+                  and finding a film you like
+                </p>
+                <p className='mb-2'>
+                  Once there, click on any image you like and click the heart
+                  icon.
+                </p>
+                <p>Here's an example image:</p>
+                <ImageCard
+                  type="poster"
+                  movie_id={772515}
+                  file_path="/1mZcxuL4GLUvPdEXC4iZPjG2EO3.jpg"
+                  aspect_ratio={0.667}
+                  height={3000}
+                  width={2000}
+                  inCollection={false}
+                />
+              </>
+            ))}
           {collection
             ?.filter((a) => a.type == 'poster')
             .sort(
               (a, b) =>
-                new Date(a.added_on).getTime() - new Date(b.added_on).getTime()
+                new Date(b.added_on).getTime() - new Date(a.added_on).getTime()
             )
             .map((entry) => (
               <ImageCard
@@ -29,14 +59,12 @@ export const CollectionPage = () => {
               />
             ))}
         </div>
-        <div
-          className="grid grid-cols-1 gap-2"
-        >
+        <div className="grid grid-cols-1 gap-2">
           {collection
             ?.filter((a) => a.type == 'backdrop')
             .sort(
               (a, b) =>
-                new Date(a.added_on).getTime() - new Date(b.added_on).getTime()
+                new Date(b.added_on).getTime() - new Date(a.added_on).getTime()
             )
             .map((entry) => (
               <ImageCard
